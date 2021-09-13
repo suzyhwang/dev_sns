60a6be83b637eceb42220b9f270631388fbae773
devSominPark <min85472@gmail.com>
2021-09-13 21:09:26 +0900

    [MainPage][Signup] 회원가입 기능 구현

---
+++ client/src/dummy/dummy.js
@@ -0,0 +1,16 @@
+export const dummy = [
+  {
+    id: 1,
+    nickName: "GiCheol",
+    email: "gicheol@naver.com",
+    password: "aA1234567!",
+    yymmdd: "1990-09-16",
+  },
+  {
+    id: 2,
+    nickName: "SuzyHwang",
+    email: "ddudi@gmail.com",
+    password: "Aa12345!",
+    yymmdd: "1994-11-10",
+  },
+];
