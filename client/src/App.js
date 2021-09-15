398e3fd39c8be09dc3922eccd37f00b58eff675c
devSominPark <min85472@gmail.com>
2021-09-15 10:35:46 +0900

    [Component][UpdatePost] 게시글 수정 모달 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-
 import React, { useState } from "react";
 import LoginForm from "./pages/LoginForm/LoginForm";
 import LogoutModal from "./components/LogoutModal/LogoutModal";
 import DeleteTweet from "./components/DeleteTweet/DeleteTweet";
 import SideBar from "./components/SideBar";
+import UpdatePost from "./components/UpdatePost/UpdatePost";
 
 function App() {
-
   return (
     <div className="App">
-      <LoginForm />
+      <UpdatePost />
+      {/* <LoginForm /> */}
       {/* <LogoutModal /> */}
       {/* <DeleteTweet /> */}
       {/* <SideBar /> */}
---
+++ client/src/components/UpdatePost/UpdatePost.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { dummy, posts } from "../../dummy/dummy";
+import {
+  Container,
+  ModalView,
+  TopContainer,
+  TilteContainer,
+  MiddleContainer,
+  ButtonContainer,
+  ProfileContainer,
+  TextContainer,
+} from "./UpdatePost.style";
+
+const UpdatePost = () => {
+  const [post, setPost] = useState(posts[0].post);
+
+  const countStr = (post) => {
+    if (post.length > 255) {
+      let newPost = post.substring(0, 255);
+      setPost(newPost);
+      console.log(post);
+    }
+  };
+
+  return (
+    <>
+      <Container>
+        <ModalView>
+          <TopContainer>
+            <TilteContainer>
+              <img
+                src={"/images/cancelBtn.svg"}
+                width="27"
+                height="31"
+                alt=""
+              ></img>
+              <div>게시물 수정</div>
+            </TilteContainer>
+          </TopContainer>
+          <MiddleContainer>
+            <ProfileContainer>
+              <img
+                src={"/images/profile.svg"}
+                width="48px"
+                height="48px"
+                alt=""
+              ></img>
+              <div>{dummy[0].nickname}</div>
+            </ProfileContainer>
+            <TextContainer>
+              <textarea
+                value={post}
+                onChange={(e) => {
+                  setPost(e.target.value);
+                  countStr(post);
+                }}
+                placeholder="What’s happening?"
+              ></textarea>
+            </TextContainer>
+          </MiddleContainer>
+          <ButtonContainer>
+            {post.length > 0 ? (
+              <button
+                onClick={() => {
+                  setPost(post);
+                  console.log(post);
+                }}
+              >
+                Tweet
+              </button>
+            ) : (
+              <div>Tweet</div>
+            )}
+          </ButtonContainer>
+        </ModalView>
+      </Container>
+    </>
+  );
+};
+
+export default UpdatePost;
